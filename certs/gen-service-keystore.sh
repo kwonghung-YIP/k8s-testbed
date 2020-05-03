073eba081380aced5fdf0b3009f15b7df7cb62e1
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2020-05-04 02:32:55 +0800

    Update gen-service-keystore.sh

--- certs/gen-service-keystore.sh
+++ certs/gen-service-keystore.sh
@@ -1,3 +1,7 @@
 openssl genrsa -out my-service.key -passout pass:abcd1234 2048
 
 openssl req -new -key my-service.key -out my-service.csr -config my-service.config
+
+openssl req -in my-service.csr -text
+
+openssl x509 -req -in my-server.csr -CA my-rootCA.crt -CAkey my-rootCA.key -CAcreateserial -out my-service.crt -days 30 -sha256
