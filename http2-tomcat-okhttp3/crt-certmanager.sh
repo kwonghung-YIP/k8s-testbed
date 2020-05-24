d7c04e56a87a473e36aa724899c3c4cab35d4332
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2020-05-25 01:35:59 +0800

    Update crt-certmanager.sh

--- http2-tomcat-okhttp3/crt-certmanager.sh
+++ http2-tomcat-okhttp3/crt-certmanager.sh
@@ -1,6 +1,6 @@
 openssl genrsa -aes-256-cbc --passout pass:abcd1234 -out my-rootCA2.key 4096
 
-openssl rsa -in my-rootCA2.key --passin pass:abcd1234 -out my-rrotCA2.out
+openssl rsa -in my-rootCA2.key --passin pass:abcd1234 -out my-rrotCA.out
 
 openssl req -verbose -x509 -new \
   -key my-rootCA.key --passin pass:abcd1234 \
