dfffe7814d71392925859f5631fd718086345f73
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2020-06-14 03:37:23 +0800

    Update istio-tomcat-verify.sh

--- http2-tomcat-okhttp3/istio-tomcat-verify.sh
+++ http2-tomcat-okhttp3/istio-tomcat-verify.sh
@@ -3,6 +3,10 @@ curl -v -HHost:tomcat-http2.hung.org.hk \
   --cacert my-rootCA.crt \
   "https://tomcat-http2.hung.org.hk:31944/echo"
 
+curl -vk -HHost:tomcat-http2.hung.org.hk \
+  --resolve "tomcat-http2.hung.org.hk:443:istio-ingressgateway.istio-system.svc.cluster.local" \
+  "https://tomcat-http2.hung.org.hk/echo"
+
 openssl s_client -showcerts \
   -servername tomcat-http2.hung.org.hk \
   -connect rancher-node02:31944 \
