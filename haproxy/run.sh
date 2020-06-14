1fb6098b8873fddeb741faa80a08b12252baa920
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2020-06-15 01:49:10 +0800

    Update run.sh

--- haproxy/run.sh
+++ haproxy/run.sh
@@ -9,4 +9,5 @@ docker run \
   --name haproxy -d --restart always \
   --network host \
   -v $PWD/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg \
+  -v $PWD/istio-gateway.pem:/usr/local/etc/haproxy/certs/istio-gateway.pem \
   haproxy:2.1
