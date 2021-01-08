f6756fb170f80dca5a640db617f5fc097253583d
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2021-01-08 17:03:04 +0800

    Update run-simple-elk.sh

--- elk-docker/run-simple-elk.sh
+++ elk-docker/run-simple-elk.sh
@@ -19,5 +19,16 @@ docker run --name kibana \
 docker run --name logstash \
   -d --restart=unless-stopped \
   --link elasticsearch:elasticsearch \
+  --volume="$(pwd)/logstash.yml:/usr/share/logstash/config/logstash.yml:ro" \
   -p 9600:9600 -p 5044:5044\
   docker.elastic.co/logstash/logstash:7.10.1 
+
+docker run --name=filebeat \
+  -d --restart=unless-stopped \
+  --user=root \
+  --volume="$(pwd)/filebeat.yml:/usr/share/filebeat/filebeat.yml:ro" \
+  --volume="/var/lib/docker/containers:/var/lib/docker/containers:ro" \
+  --volume="/var/run/docker.sock:/var/run/docker.sock:ro" \
+  --link logstash:logstash \
+  docker.elastic.co/beats/filebeat:7.10.1 \
+  filebeat -e -strict.perms=false
