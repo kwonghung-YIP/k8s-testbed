1d2be6d89dc85bf85a9336eea8e9e1b4ae1b531b
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2021-01-15 16:06:55 +0800

    Update run-simple-elk.sh

--- elk-docker/run-simple-elk.sh
+++ elk-docker/run-simple-elk.sh
@@ -9,13 +9,13 @@ docker rm -f springboot-webapp
 docker rm -f nginx
 
 docker run --name elasticsearch \
-  -d --restart=unless-stopped \
+  -d --restart=always \
   -p 9200:9200 -p 9300:9300 \
   -e "discovery.type=single-node" \
   docker.elastic.co/elasticsearch/elasticsearch:7.10.1
 
 docker run --name kibana \
-  -d --restart=unless-stopped \
+  -d --restart=always \
   --link elasticsearch:elasticsearch \
   -p 5601:5601 \
   docker.elastic.co/kibana/kibana:7.10.1
